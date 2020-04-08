72080d96600492412917cce0c9c243e74a5de0c0
4ferre <dfgergx@laposte.net>
2020-04-08 14:09:37 +0200

    acceuil

--- Brick Shooter/Jeu/Application.py
+++ Brick Shooter/Jeu/Application.py
@@ -13,8 +13,10 @@ class Application :
         pygame.display.set_caption("ISN ILIES")
  
         self.fond = self.config.getBlue()
+        
  
         self.fenetre = pygame.display.set_mode((self.config.getSurfaceW(),self.config.getSurfaceH()),pygame.FULLSCREEN)#pygame.display.set_mode((surfaceW,surfaceH))
+        self.fenetre.blit(pygame.image.load("images/acceuil.jpg"),(0,0))
         # Groupe de sprites utilisé pour l'affichage
         self.groupeGlobal = pygame.sprite.Group()
         self.statut = True
@@ -67,7 +69,7 @@ class Application :
                 self.quitter()
                 return
  
-        self.fenetre.fill(self.fond)
+       # self.fenetre.fill(self.fond)
         self.ecran.update(events)
         self.groupeGlobal.update()
         self.groupeGlobal.draw(self.fenetre)
